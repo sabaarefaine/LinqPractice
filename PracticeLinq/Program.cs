733f9b387dcce65ceb6209da6b3b438ba91d660a
Saba Arefaine <arefainesaba@gmail.com>
2021-04-21 13:01:09 -0700

    sorting list with linq

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> favoriteShows = new List<string>
+            {"Bobs Burgers", "Handmaids Tale", "The Office", "Curb Your Enthusiasm", "Real Housewives"};
+
+
+            IEnumerable<string> sortedShows = favoriteShows.OrderBy(x => x.Length);
+
+            foreach (var show in sortedShows)
+            {
+            Console.WriteLine(show);
+            }
+
+
+            //another way i found to order by
+            /*
+            var sortedShows = from show in favoriteShows
+                              orderby show.Length
+                              select show;
+
+            foreach (var x in sortedShows)
+            {
+                Console.WriteLine(x);
+            }
+            */
+        }
+    }
+}
